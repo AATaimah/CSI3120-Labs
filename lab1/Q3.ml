ada3d848bf8e38263ecab4dd59fc149774b3074b
AATaimah <129572284+AATaimah@users.noreply.github.com>
2024-09-20 10:31:21 -0400

    Q3

--- lab1/Q3.ml
+++ lab1/Q3.ml
@@ -1,1 +1,8 @@
-this is the answer (its not)
+let compose_functions f g =
+  fun x -> f (g x)
+
+let composed = compose_functions (fun x -> x * 2) (fun y -> y + 3)
+
+let () = 
+  let result = composed 5 in
+  Printf.printf "Result: %d\n" result
