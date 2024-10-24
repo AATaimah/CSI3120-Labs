da9fe0c1ccd6eada815c2807cbff8e15fba7ddc6
omarabdul <omaraabdul9@gmail.com>
2024-10-24 10:15:07 -0400

    part 3

--- lab5/lab5.pl
+++ lab5/lab5.pl
@@ -1,3 +1,5 @@
+/* Task 1 */
+
 /* Parent-child relationships */
 parent(john, mary).
 parent(john, tom).
@@ -33,6 +35,9 @@ ancestor(Anc, Desc) :-
     ancestor(P, Desc).
 
 
+
+
+/* Task 2 */
 % Base case: an empty list has a sum of 0
 sum_odd_numbers([], 0).
 
@@ -46,3 +51,36 @@ sum_odd_numbers([Head|Tail], Sum) :-
 sum_odd_numbers([Head|Tail], Sum) :-
     Head mod 2 =:= 0,  % Check if Head is even
     sum_odd_numbers(Tail, Sum).  % Continue with tail
+
+
+/* Task 3 */
+
+
+solve_puzzle(Houses, GreenIndex) :-
+    % Hint: Use the built-in Prolog Predicate to define all the possible combinations of the houses.
+    permutation([red, blue, green, yellow], Houses),
+    
+    % Clue 1: The red house is immediately to the left of the blue house
+    immediately_left_of(red, blue, Houses),
+    
+    % Clue 2: The treasure is in the green house
+    nth1(GreenIndex, Houses, green),
+    
+    % Clue 3: The yellow house is not next to the green house
+    not_next_to(yellow, green, Houses),
+    
+    % Clue 4: The green house is not in the second position from the left
+    GreenIndex \= 2.
+
+% Helper predicate to check if one element is immediately to the left of another
+immediately_left_of(Left, Right, List) :-
+    append(_, [Left, Right | _], List).
+
+% Helper predicate to check if two elements are next to each other
+next_to(X, Y, List) :-
+    append(_, [X, Y | _], List);
+    append(_, [Y, X | _], List).
+
+% Helper predicate to check if two elements are not next to each other
+not_next_to(X, Y, List) :-
+    \+ next_to(X, Y, List).
